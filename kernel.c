898b892faadd374bc938ffd864e21910bb7b5032
Juul <juul20018@gmail.com>
2025-11-27 16:55:17 +0100

    starting the kernel (implementing VGA colors)

---
+++ kernel.c
@@ -0,0 +1,53 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+#define VGA_MEMORY 0xB8000
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer = (uint16_t*)VGA_MEMORY;
+
+/* 8-bit vga colors */
+enum vga_color {
+	VGA_COLOR_BLACK 	= 0,
+	VGA_COLOR_BLUE		= 1,
+	VGA_COLOR_GREEN 	= 2,
+	VGA_COLOR_CYAN		= 3,
+	VGA_COLOR_RED		= 4,
+	VGA_COLOR_MAGENTA	= 5,
+	VGA_COLOR_BROWN		= 6,
+	VGA_COLOR_LIGHT_GREY	= 7,
+	VGA_COLOR_DARK_GREY	= 8,
+	VGA_COLOR_LIGHT_BLUE	= 9,
+	VGA_COLOR_LIGHT_GREEN	= 10,
+	VGA_COLOR_LIGHT_CYAN	= 11,
+	VGA_COLOR_LIGHT_RED	= 12,
+	VGA_COLOR_LIGHT_MAGENTA	= 13,
+	VGA_COLOR_LIGHT_YELLOW	= 14,
+	VGA_COLOR_WHITE		= 15
+};
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+	return fg | bg << 4;
+}
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
+
+void terminal_init(void) {
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t idx = y * VGA_WIDTH + x;
+			terminal_buffer[idx] = vga_entry(' ', terminal_color);
+		}
+	}
+}
