3de46888401918b5370891fee8f5838414771fe2
Juul <juul20018@gmail.com>
2025-12-19 23:06:09 +0100

    adding header file for minimal string.h

---
+++ libc/include/string.h
@@ -0,0 +1,23 @@
+#ifndef _STRING_H
+#define _STRING_H
+
+#include <sys/cdefs.h>
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+size_t strl(const char*);
+int memcmp(const void*, const void*, size_t);
+void* memcpy(void* __restrict, const void* __restrict, size_t);
+void* memmove(void*, const void*, size_t);
+void* memset(void*, int, size_t);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
