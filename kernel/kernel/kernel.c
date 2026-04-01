387159c3bda1cf083b47be0e5c21741fc24bb71f
Juul <juul20018@gmail.com>
2026-04-01 15:09:40 +0200

    implement test for memcpy and fix \n character

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <kernel/tty.h>
 
 void kernel_main(void) {
 	terminal_init();
-    terminal_writestring("Hello from terminal!");
+    terminal_writestring("Hello from terminal!\n");
+
+    // Testing memcpy
+    char src[] = "test";
+    char dest[10];
+    memcpy(dest, src, 5);
+
+    terminal_writestring("Source: ");
+    terminal_writestring(src);
+    terminal_writestring("\n");
+
+    terminal_writestring("Destination: ");
+    terminal_writestring(dest);
+    terminal_writestring("\n");
 }
