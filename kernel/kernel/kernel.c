17a003e5f927b3cf72e68a05f46a175a3276d47a
Juul <juul20018@gmail.com>
2026-05-21 23:41:16 +0200

    feat(multiboot): added functionality to get multiboot info

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,14 +1,17 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
 #include <kernel/tty.h>
 #include <kernel/serial.h>
 
+#define MULTIBOOT_MAGIC 0x2BADB002
+
 #ifdef RUN_TESTS
 #include "tests/test_runner.h"
 #endif
 
-void kernel_main(void) {
+void kernel_main(uint32_t magic, uint32_t mbi_addr) {
 	terminal_init();
 
     if (serial_init() != 0) {
@@ -16,6 +19,15 @@ void kernel_main(void) {
     } else {
         printf("Serial initialized\n");
     }
+
+    if (magic != MULTIBOOT_MAGIC) {
+        printf("Not booted by Multiboot!\n");
+        printf("magic: 0x%x\n", magic);
+        for(;;);
+    }
+
+    printf("Booted correctly, magic: 0x%x\n", magic);
+    printf("Multiboot info at: 0x%x\n", mbi_addr);
 
     #ifdef RUN_TESTS
         run_all_tests();
